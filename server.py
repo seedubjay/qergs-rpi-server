0d965996a2260a5d7bd1e9524a38799bbfc08b19
seedubjay <cwj@dhcp-10-249-79-225.eduroam.wireless.private.cam.ac.uk>
2020-10-22 14:25:55 +0100

    change port to 5353

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from pyrow import pyrow
+from pyrow import simpyrow as pyrow
 from pyrow.ergmanager import ErgManager
 
 from flask import Flask, json, request
@@ -6,8 +6,13 @@ from flask_cors import CORS, cross_origin
 
 from datetime import datetime, time, timedelta, date
 import random
+import sys
 
 data = {}
+ergman = None
+
+port = 5353
+if len(sys.argv) > 1: port = int(sys.argv[1])
 
 def add_cb(erg):
     pass
@@ -17,13 +22,13 @@ def change_cb(erg):
     description = erg._pyerg.get_erg()
     data = {
         "id": description["serial"],
-	"status": description["status"],
-	"time": erg.data["time"],
-	"distance": erg.data["distance"],
-	"interval_count": erg.data["intcount"],
-	"workout_state": erg.data["state"],
-	"pace": erg.data["pace"],
-	"rate": erg.data["spm"]
+        "status": description["status"],
+        "time": erg.data["time"],
+        "distance": erg.data["distance"],
+        "interval_count": erg.data["intcount"],
+        "workout_state": erg.data["state"],
+        "pace": erg.data["pace"],
+        "rate": erg.data["spm"]
     }
 
 api = Flask(__name__)
@@ -32,11 +37,15 @@ api.config['CORS_HEADERS'] = 'Content-Type'
 
 @api.route('/', methods=['GET'])
 @cross_origin()
-def get_standings():
+def get_state():
     global data
-    print(data)
     return json.dumps(data)
+
+@api.route('/workout', methods=['POST'])
+@cross_origin()
+def set_workout():
+    if ergman is not None: ergman.set_workout(**request.form)
 
 if __name__ == '__main__':
     ergman = ErgManager(pyrow, add_callback=add_cb, update_callback=change_cb)
-    api.run(host='0.0.0.0')
+    api.run(host='0.0.0.0', port=port)
